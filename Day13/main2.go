a45763de6509e9cb4ceb505929aa790782fe93ec
Rayanry9 <rayanhldr@gmail.com>
2024-12-15 20:53:07 +0530

    Day 13

---
+++ Day13/main2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("./input.txt")
+	scanner := bufio.NewScanner(file)
+	counter := 0
+	var aX, aY, bX, bY, finalX, finalY int
+	var value int
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			counter = 0
+			continue
+		}
+
+		spaceSep := strings.Split(line, " ")
+		if counter == 0 {
+			aY, _ = strconv.Atoi(spaceSep[3][2:])
+			spaceSep = strings.Split(spaceSep[2], ",")
+			aX, _ = strconv.Atoi(spaceSep[0][2:])
+		} else if counter == 1 {
+			bY, _ = strconv.Atoi(spaceSep[3][2:])
+			spaceSep = strings.Split(spaceSep[2], ",")
+			bX, _ = strconv.Atoi(spaceSep[0][2:])
+		} else if counter == 2 {
+			finalY, _ = strconv.Atoi(spaceSep[2][2:])
+			spaceSep = strings.Split(spaceSep[1], ",")
+			finalX, _ = strconv.Atoi(spaceSep[0][2:])
+
+			// Implementation
+
+			finalX += 10000000000000
+			finalY += 10000000000000
+
+			d := (aX * bY) - (bX * aY)
+			dA := (finalX * bY) - (finalY * bX)
+			dB := (aX * finalY) - (aY * finalX)
+
+			if !(d == 0 && dA != 0 && dB != 0) {
+				if dA%d == 0 && dB%d == 0 {
+					value += ((dA / d) * 3) + (dB / d)
+				}
+			}
+		}
+		counter++
+	}
+	fmt.Println(value)
+}
